f4eb10579e261cea774311e1d64313d717849bcd
Arman Moaveni <134084996+arimoa@users.noreply.github.com>
2023-06-22 02:29:45 +0330

    Update main.js

--- main.js
+++ main.js
@@ -6,22 +6,48 @@ var amount, rate, months, result;
 
 amountEl.addEventListener("input", (e) => {
   amount = e.target.value;
-  result = (amount * (1 + rate / 100)) / months;
-  if (rateEl.value !== " " && monthsEl.value !== "") {
+
+  if (amount > 0) {
+    result = (amount * (1 + rate / 100)) / months;
+  } else {
+    window.alert("Loan amount can not be lower than 1$");
+    amount = 1;
+    e.target.value = 1;
+    result = (amount * (1 + rate / 100)) / months;
+  }
+  if (monthsEl.value !== " " && rateEl.value !== "") {
     resultEl.innerHTML = result.toFixed(2) + " $";
   }
 });
+
 rateEl.addEventListener("input", (e) => {
   rate = e.target.value;
-  result = (amount * (1 + rate / 100)) / months;
-  if (amountEl.value !== " " && monthsEl.value !== "") {
+
+  if (rate <= 100 && rate >= 0) {
+    result = (amount * (1 + rate / 100)) / months;
+  } else {
+    window.alert("percentage can not be lower than 0% or higher than 100%");
+    rate = 0;
+    e.target.value = 0;
+    result = (amount * (1 + rate / 100)) / months;
+  }
+  if (amountEl.value !== " " && months.value !== "") {
     resultEl.innerHTML = result.toFixed(2) + " $";
   }
 });
+
 monthsEl.addEventListener("input", (e) => {
   months = e.target.value;
-  result = (amount * (1 + rate / 100)) / months;
-  if ((amountEl.value !== " ") & (rateEl.value !== "")) {
+
+  if (months >= 1) {
+    result = (amount * (1 + rate / 100)) / months;
+  } else {
+    window.alert("months can not be lower than 1");
+    months = 1;
+    e.target.value = 1;
+    result = (amount * (1 + rate / 100)) / months;
+  }
+  if (amountEl.value !== " " && rateEl.value !== "") {
     resultEl.innerHTML = result.toFixed(2) + " $";
   }
 });
